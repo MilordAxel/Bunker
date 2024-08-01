25bd1651a2c9bf302726f76082c37e0668a9322e
MilordAxel <alexbouchenko03@gmail.com>
2024-08-01 19:57:47 +0300

    create basic client part

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/home/Home"
+
+function App() {
+    return (
+        <>
+            <Routes>
+            </Routes>
+        </>
+    )
+}
+
+export default App
