6f8350b33bbf2f72c44aed18e6c84c5712084376
MilordAxel <alexbouchenko03@gmail.com>
2024-11-28 13:10:28 +0200

    add cached model Player

--- server/game/models.py
+++ server/game/models.py
@@ -1,3 +1,6 @@
+from string import ascii_letters, digits
+from random import choice, randint
+
 from django.db import models
 from django.core.validators import (
     RegexValidator
@@ -12,3 +15,69 @@ class Characteristic(models.Model):
         ]
     )
     value = models.CharField(max_length=255)
+
+
+def gen_age(): return randint(18, 90)
+def gen_gender(): return choice(["male", "female"])
+
+def gen_game_code():
+    return "".join(
+        [ choice(ascii_letters + digits) for i in range(10) ]
+    )
+
+class GenCharacteristic:
+    def __init__(self, characteristic_type):
+        self.__type = characteristic_type
+
+    def __call__(self):
+        return choice(Characteristic.objects.filter(type=self.__type))
+
+
+# Cached model
+class Player(models.Model):
+    nickname = models.CharField(max_length=40)
+
+    # Characteristics
+    age = models.IntegerField(
+        default=gen_age
+    )
+    gender = models.CharField(
+        max_length=6,
+        default=gen_gender
+    )
+    physique = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("physique")
+    )
+    profession = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("profession")
+    )
+    disease = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("disease")
+    )
+    fear = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("fear")
+    )
+    hobby = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("hobby")
+    )
+    inventory = models.ForeignKey(
+        Characteristic,
+        on_delete=models.CASCADE,
+        related_name="+",
+        default=GenCharacteristic("inventory")
+    )
