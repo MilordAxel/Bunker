b024ba15b4d68cff4d489c30a171e2a761a064bd
MilordAxel <alexbouchenko03@gmail.com>
2024-12-12 13:55:11 +0200

    create serializer for cached model Player

---
+++ server/game/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from . import models
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Player
+        exclude = ["id"]
+        depth = 1
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        characteristic_types = [
+            "physique", "profession", "disease", "fear", "hobby", "inventory"
+        ]
+        for type in characteristic_types:
+            representation[type] = getattr(instance, type).value
+        
+        return representation
+
