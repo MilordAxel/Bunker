d32d599f4801bd8daaa7de3e9acd653d692a7872
MilordAxel <alexbouchenko03@gmail.com>
2025-01-28 15:56:56 +0200

    change env variable name for variable which store server url for http requests

--- client/src/instances/axios.jsx
+++ client/src/instances/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const SERVER_URL = import.meta.env.VITE_SERVER_HTTP_URL;
 
 const axiosInstance = axios.create({
     baseURL: SERVER_URL
