5906a0ee793fc2fe9748ef80f8466a38477be69c
MilordAxel <alexbouchenko03@gmail.com>
2026-02-20 17:00:06 +0200

    add JoinGame route to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import {Routes, Route} from "react-router-dom"
 import Home from "./pages/home/Home"
 import Support from "./pages/support/Support"
 import NewGame from "./pages/NewGame/NewGame"
+import JoinGame from "./pages/JoinGame/JoinGame"
 import GameWaiting from "./pages/GameWaiting/GameWaiting"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                 <Route element={<Home/>} path="/" />
                 <Route element={<Support />} path="/support" />
                 <Route element={<NewGame />} path="/new_game" />
+                <Route element={<JoinGame />} path="/join_game" />
                 <Route element={<GameWaiting />} path="/game_waiting/:gameCode" />
             </Routes>
         </>
